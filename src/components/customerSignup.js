bc9db44e50b9e1780672ae3d55ca0704ead3b0e3
MrMz <amobarez00@gmail.com>
2021-11-16 18:25:42 +0330

    add input handlers for customer sign up

--- src/components/customerSignup.js
+++ src/components/customerSignup.js
@@ -4,10 +4,39 @@ import { Link } from 'react-router-dom'
 
 export const CustomerSignup = (props) => {
     const [inputs, setInputs] = useState({ username: '', password: '', dupPassword: '', mobile: '' })
+    const [validated, setValidated] = useState(false)
     const { username, password, dupPassword, mobile } = inputs
 
+    const mobileHandler = value => {
+        value = value.trim()
+        let res = value.length === 11 && value[0] === "0" && value[1] === "9"
+        let hasSpace = /\s/g.test(value)
+        return (res && !hasSpace) ? value : ''
+    }
+
+    const passwordHandler = value => {
+        let hasMinLength = value.length > 4 ? value : false
+
+        return hasMinLength ? value : ''
+    }
+
+    const usernameHandler = value => {
+        let hasSpace = /\s/g.test(value)
+        let hasMinLength = value.length > 3 ? true : false
+        return (!hasSpace && hasMinLength) ? value : ''
+    }
+
     const handleChange = e => {
-        const { id, value } = e.target
+       let { id, value } = e.target
+
+        if(id === "username")
+            value = usernameHandler(value)
+        if(id === "password" || id === "dupPassword")
+            value = passwordHandler(value)
+        if(id === "mobile")
+            value = mobileHandler(value)
+            
+        
         setInputs({...inputs, [id]: value})
     }
     
@@ -15,10 +44,12 @@ export const CustomerSignup = (props) => {
         e.preventDefault()
 
         let user = { username, password, mobile }
+        console.log(inputs)
 
-        username && password === dupPassword && mobile && 
-            console.log('time for calling api') 
-        console.log('form handler')
+        username && password === dupPassword && mobile 
+        ? console.log('time for calling api') 
+        : setValidated(true)
+        
     }
 
     return (
@@ -33,13 +64,16 @@ export const CustomerSignup = (props) => {
                     <h5 className="w-100 text-center">ثبت نام مشتریان:</h5>
                 </Modal.Header>
                 <Modal.Body>
-                    <Form onSubmit={formHandler}>
+                    <Form noValidate onSubmit={formHandler}>
                         <Form.Group className="mb-3" controlId="username">
                             <Form.Label className="me-2">نام کاربری</Form.Label>
                             <Form.Control 
                                 type="text" 
                                 placeholder="نام کاربری خود را بدون فاصله وارد کنید" 
                                 onChange={handleChange} 
+                                isInvalid={!username && validated && true}
+                                isValid={username && validated && true} 
+                                required
                             />
                         </Form.Group>
 
@@ -51,6 +85,9 @@ export const CustomerSignup = (props) => {
                                         type="password" 
                                         placeholder="رمز عبور باید حداقل ۵ کاراکتر باشد" 
                                         onChange={handleChange} 
+                                        isInvalid = {!password && validated && true}
+                                        isValid = {password && validated && true}
+                                        required
                                     />
                                 </Form.Group>
                             </Col>                        
@@ -61,6 +98,9 @@ export const CustomerSignup = (props) => {
                                         type="password" 
                                         placeholder="رمز عبور خود را دوباره وارد کنید" 
                                         onChange={handleChange} 
+                                        isValid = {password === dupPassword && dupPassword.length && validated && true}
+                                        isInvalid = {password != dupPassword && validated && true}
+                                        required
                                     />
                                 </Form.Group>
                             </Col> 
@@ -70,8 +110,11 @@ export const CustomerSignup = (props) => {
                             <Form.Label className="me-2">شماره همراه</Form.Label>
                             <Form.Control 
                                 type="number" 
-                                placeholder="09021234567" 
-                                onChange={handleChange} 
+                                placeholder="مثال: 09021234567" 
+                                onChange={handleChange}
+                                isValid = {mobile && validated && true}
+                                isInvalid = {!mobile && validated && true} 
+                                required
                             />
                         </Form.Group>
                         <Button className="m-auto d-block w-25" variant="primary" type="submit">
