37ae3e9d65f44b1355479068057ea445fa77fa29
MrMz <amobarez00@gmail.com>
2021-11-16 18:42:20 +0330

    add redux and store

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+
+import { store } from './helpers'
+import { Provider } from 'react-redux'; 
+
 import './assets/styles/myBootstrap.css'
 import './assets/styles/main.css'
-
 import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
